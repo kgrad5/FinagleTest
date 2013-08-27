4d3406a409cdcc7e1558ac68560ebbe1b3d8ae80
Kevin Grad <kgrad5@gmail.com>
2013-08-26 20:55:43 -0400

    added a list option that lists the filesystem

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -12,13 +12,23 @@ import java.nio.charset.Charset
 
 object Server extends App {
  
+  def respond(res: String): HttpResponse = {
+    val response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
+        	response.setContent(ChannelBuffers.copiedBuffer(res, Charset.forName("UTF-8")))     
+        	response
+  }
+  
   def handler(req: HttpRequest): HttpResponse = {
     req.getUri() match {
     	case "/" =>  new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
         case "/hello" => { 
         	val response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK)
-        	response.setContent(ChannelBuffers.copiedBuffer("Hello, World!", Charset.forName("UTF-8")))     
-        	response
+        	respond("Hello, World!")
+        }
+        case "/list" => {
+          val f: java.io.File = new java.io.File(".")
+          val files: String = f.listFiles() map ({ x => if (x.isDirectory()) x.getName() + "/" else x.getName()}) reduce (_ + "\n" + _)
+          respond(files)
         }
         case _ => new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_REQUEST)
      }
@@ -30,6 +40,8 @@ object Server extends App {
       Future.value(response)
     }    
   }
+  
+  
   val server = Http.serve(":8080", service)
   Await.ready(server)
 }
